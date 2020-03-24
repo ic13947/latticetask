4092eb34316ca5af983d27f259952db52976ec80
Ian Cooper <iancoopercoaching@gmail.com>
2020-03-24 08:55:50 +0100

    First working version
    The function furthest_from_average returns a list of all routes with
    grade furthest from mean, along with their grades, since there may be
    multiple routes with grade furthest from mean and they may have
    different grades. e.g consider the four entry logbook with grades 6c,
    6c, 7b, 7b.

--- main.py
+++ main.py
@@ -1,2 +1,87 @@
-def analyse_logbook():
-    pass
+import csv
+from statistics import mean
+from collections import namedtuple
+
+# GRADE_TO_NUMBER: dict for converting French grades to numeric grades
+GRADE_TO_NUMBER = {}
+
+with open('grade_table.csv') as csvfile:
+    grade_reader = csv.reader(csvfile)
+    for row in grade_reader:
+        GRADE_TO_NUMBER[row[0]] = int(row[1])
+
+# we also want the inverse to convert numeric grades back to French grades:
+NUMBER_TO_GRADE = {value: key for key, value in GRADE_TO_NUMBER.items()}
+
+# logbook_entries: list of named tuples of form [(route name, grade, tries)]
+LogbookEntry = namedtuple('LogbookEntry', ['route_name', 'grade', 'attempts'])
+
+
+def number_of_unique_routes(logbook_entries):
+    # note that a set {} is a list of *unique* elements
+    return(len({entry.route_name for entry in logbook_entries}))
+
+
+def unsuccessful_attempts(logbook_entries):
+    successful_attempts = len(logbook_entries)
+    total_attempts = sum([entry.attempts for entry in logbook_entries])
+    unsuccessul_attempts = total_attempts - successful_attempts
+    return(unsuccessul_attempts)
+
+
+def mean_grade(logbook_entries):
+    numeric_grades = [GRADE_TO_NUMBER[entry.grade] for entry in logbook_entries]
+    # int always rounds down so use int(X+0.5) for nearest integer to X
+    mean_numeric_grade = int(sum(numeric_grades) / len(numeric_grades) + 0.5)
+    return(NUMBER_TO_GRADE[mean_numeric_grade])
+
+
+def hardest_route(logbook_entries):
+    numeric_grades = [GRADE_TO_NUMBER[entry.grade] for entry in logbook_entries]
+    return(NUMBER_TO_GRADE[max(numeric_grades)])
+
+
+def furthest_from_average(logbook_entries):
+    mean_numeric_grade = mean([GRADE_TO_NUMBER[entry.grade] for entry in logbook_entries])
+# delta is the difference between the numeric grade and the mean numeric grade
+    logbook_entries_with_deltas = [
+        (abs(GRADE_TO_NUMBER[entry.grade] - mean_numeric_grade), entry)
+        for entry in logbook_entries
+    ]
+    maxdelta = max(logbook_entries_with_deltas)[0]
+# Note that there may be more than one route with grade furthest from the mean.
+# Also, if the mean turns out to be an integer (only likely for short logbooks)
+# there may be some routes the same distance above the mean as below the mean.
+# For example, consider the three entry logbook with grades 6c, 7a, 7b.
+# Both the routes with grades 6c and 7b would be furthest from the mean.
+# Therefore, we want to return a list of all the routes with grade furthest
+# from the mean.
+    return([
+        (entry[1].route_name, entry[1].grade)
+        for entry in logbook_entries_with_deltas if entry[0] == maxdelta
+    ])
+
+
+def analyse_logbook(logbook_entries):
+    return([number_of_unique_routes(logbook_entries),
+           unsuccessful_attempts(logbook_entries),
+           mean_grade(logbook_entries),
+           hardest_route(logbook_entries),
+           furthest_from_average(logbook_entries)])
+def main():
+    logbook_entries = []
+
+    with open('logbook.csv') as csvfile:
+        logbook_reader = csv.reader(csvfile)
+        for row in logbook_reader:
+            *row, attempts = row
+            logbook_entries.append(LogbookEntry(*row, int(attempts)))
+
+    print(number_of_unique_routes(logbook_entries))
+    print(unsuccessful_attempts(logbook_entries))
+    print(mean_grade(logbook_entries))
+    print(hardest_route(logbook_entries))
+    print(furthest_from_average(logbook_entries))
+
+if __name__ == '__main__':
+    main()
--- test_main.py
+++ test_main.py
@@ -1,20 +1,55 @@
-from main import analyse_logbook
+import csv
+from main import (
+    analyse_logbook,
+    unsuccessful_attempts,
+    mean_grade, hardest_route,
+    furthest_from_average,
+    number_of_unique_routes,
+    LogbookEntry
+    )
 
 
-def test_something():
-    logbook_entries = [
-        ("Super furry animal", "7a+", 1),
-        ("Rescate", "7b+", 3),
-        ("Super furry animal", "7a+", 1),
-        ("Amigo", "5+", 1)
-    ]
+TEST_LOGBOOK_ENTRIES = [
+    ('Super furry animal', '7a+', 1),
+    ("Rescate", "7b+", 3),
+    ("Super furry animal", "7a+", 1),
+    ("Amigo", "5+", 1)
+]
 
+TEST_LOGBOOK_ENTRIES = [LogbookEntry(*entry) for entry in TEST_LOGBOOK_ENTRIES]
+
+
+def test_number_of_unique_routes():
+    expected_output = 3
+    assert number_of_unique_routes(TEST_LOGBOOK_ENTRIES) == expected_output
+
+
+def test_unsuccessful_attempts():
+    expected_output = 2
+    assert unsuccessful_attempts(TEST_LOGBOOK_ENTRIES) == expected_output
+
+
+def test_mean_grade():
+    expected_output = '7a'
+    assert mean_grade(TEST_LOGBOOK_ENTRIES) == expected_output
+
+
+def test_hardest_route():
+    expected_output = '7b+'
+    assert hardest_route(TEST_LOGBOOK_ENTRIES) == expected_output
+
+
+def test_furthest_from_average():
+    expected_output = [('Amigo', '5+')]
+    assert furthest_from_average(TEST_LOGBOOK_ENTRIES) == expected_output
+
+
+def test_analyse_logbook():
     expected_output = [
         3,
         2,
-        "7a",
-        "7b+",
-        "Amigo",
+        '7a',
+        '7b+',
+        [('Amigo','5+')],
     ]
-
-    assert analyse_logbook(logbook_entries) == expected_output
+    assert analyse_logbook(TEST_LOGBOOK_ENTRIES) == expected_output
